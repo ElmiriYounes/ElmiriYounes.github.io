8fe3beda079e3c29a7371cd5e5c1701e5e02bf1b
ElmiriYounes <elmiri.younes@hotmail.com>
2022-06-02 17:02:58 +0200

    hangman game

---
+++ hangman/ReadMe.md
@@ -0,0 +1,13 @@
+# HANGMAN GAME
+
+![hangman game]('https://d338t8kmirgyke.cloudfront.net/icons/icon_pngs/000/001/955/original/hangman.png')
+
+Hangman is a guessing game for two or more players. One player thinks of a word, phrase or sentence and the other(s) tries to guess it by suggesting letters within a certain number of guesses.
+
+## History
+Though the origins of the game are unknown, a variant is mentioned in a book of children's games assembled by Alice Gomme in 1894 called Birds, Beasts, and Fishes.[1] This version lacks the image of a hanged man, instead relying on keeping score as to the number of attempts it took each player to fill in the blanks.
+
+A version which incorporated hanging imagery was described in a 1902 Philadelphia Inquirer article, which stated that it was popular at "White Cap" parties hosted by "Vigilance Committees" where guests would wear "white peaked caps with masks".
+
+## link of website
+[hangman game](https://ElmiriYounes.io/hangman)
---
+++ hangman/script.js
@@ -0,0 +1,81 @@
+import { words } from "./wordsList.js";
+
+const word = words[Math.floor(Math.random() * words.length)];
+const WordContainer = document.querySelector(".word-container");
+
+for (let char of word) {
+  const letterWordContainer = document.createElement("li");
+  letterWordContainer.classList.add("letter-word-container");
+  const letterWord = document.createElement("span");
+  letterWord.classList.add("letter-word", "letter-word-hidden");
+  letterWord.innerHTML = char;
+  letterWordContainer.appendChild(letterWord);
+  WordContainer.appendChild(letterWordContainer);
+}
+
+const keyboard = document.querySelector(".keyboard");
+let char = "a";
+for (let i = 0; i <= 25; i++) {
+  const letterKeyboard = document.createElement("button");
+  letterKeyboard.classList.add("letter-keyboard");
+  setTimeout(() => {
+    letterKeyboard.classList.add("letter-keyboard-scaling");
+  }, i * 100);
+  letterKeyboard.innerHTML = char;
+  keyboard.appendChild(letterKeyboard);
+  char = String.fromCharCode(char.charCodeAt(0) + 1);
+}
+
+let lives = 6;
+const body = document.querySelector(".man");
+const membersBody = document.querySelectorAll(".member-body");
+let indexMembersBody = 0;
+
+let countLetterFound = 0;
+const lettersKeyboard = document.querySelectorAll(".letter-keyboard");
+const lettersWord = document.querySelectorAll(".letter-word-container");
+const msgEnd = document.querySelector(".msg-end");
+const livesTarget = document.querySelector(".lives");
+for (let letter of lettersKeyboard) {
+  letter.addEventListener("click", (e) => {
+    let letterFound = false;
+    for (let i in word) {
+      word[i] === e.target.innerHTML.toLowerCase() &&
+        (lettersWord[i].firstChild.classList.remove("letter-word-hidden"),
+        (letterFound = true),
+        countLetterFound++);
+    }
+
+    if (!letterFound) {
+      switch (indexMembersBody) {
+        case 2:
+        case 4:
+          membersBody[indexMembersBody].classList.add("apears-arms-foot-right");
+          break;
+        case 3:
+        case 5:
+          membersBody[indexMembersBody].classList.add("apears-arms-foot-left");
+          break;
+        default:
+          membersBody[indexMembersBody].classList.add("apears");
+          break;
+      }
+      indexMembersBody++;
+      livesTarget.innerHTML = `Tentatives: ${--lives}`;
+      e.target.classList.add("not-found");
+    } else {
+      e.target.classList.add("found");
+    }
+
+    e.target.disabled = true;
+
+    (lives === 0 || countLetterFound === word.length) &&
+      ((keyboard.style.zIndex = "-1"),
+      (msgEnd.style.display = "block"),
+      msgEnd.classList.add("apears-msg-end")),
+      lives === 0 &&
+        ((msgEnd.innerHTML = `game over ! the hidden word: ${word}</br><a href="/"><button class="reset">Restart</button></a>`),
+        (msgEnd.style.color = "red"),
+        body.classList.add("hang"));
+  });
+}
