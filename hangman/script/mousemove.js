5d689aa88fbf2874830d42daaffe873de4b990e3
ElmiriYounes <elmiri.younes@hotmail.com>
2022-06-15 10:28:10 +0200

    r

--- hangman/script/mousemove.js
+++ hangman/script/mousemove.js
@@ -14,9 +14,17 @@
   document.body.addEventListener(
     "mousemove",
     (e) => {
-      circle.style.top = e.clientY - 50 + "px";
-      circle.style.left = e.clientX - 50 + "px";
-      moveCircle("not hide");
+        // by default with mousemove, in mobile if we touchstart the screen the circle appears...
+        // navigator.maxTouchPoints return 1 if touch finger is detected or 0 if not detected
+        // if navigator.maxTouchPoints == 0 => cursor move in mobile
+        // if navigator.maxTouchPoints == -1 => touch finger
+        // if navigator.maxTouchPoints == 256 => cursor move in desktop
+        console.log(navigator.maxTouchPoints)
+      if (navigator.maxTouchPoints === 0  || navigator.maxTouchPoints === 256) {
+        circle.style.top = e.clientY - 50 + "px";
+        circle.style.left = e.clientX - 50 + "px";
+        moveCircle("not hide");
+      }
     },
     false
   );
@@ -28,8 +36,4 @@
     },
     false
   );
-
-  document.body.addEventListener("touchstart", () => {
-    moveCircle("hide");
-  });
 })();
